c5fb2854986acacdf7c7a43dac261e0c0d8505fd
Amitkm997 <amitkm997@gmail.com>
2026-07-28 14:14:50 +0530

    company controller

--- backend/routes/companyRoute.js
+++ backend/routes/companyRoute.js
@@ -1,8 +1,17 @@
-import express from 'express';
-import { addCompany } from '../controllers/companyController.js';
 
-const router = express.Router();
 
-router.post("/",addCompany)
+import express from 'express'
+import { addCompany, applyCompanies, deleteCompany, getAllCompanies,getCompanyByid, updateCompany } from '../controllers/companyController.js';
+import {verifyToken} from '../middleware/auth.js'
+import { isAdmin } from '../middleware/isAdmin.js';
+const router=express.Router();
 
+router.post('/',verifyToken,isAdmin,addCompany)
+router.get('/',getAllCompanies);
+router.get('/:id',getCompanyByid)
+
+router.put('/',verifyToken,isAdmin,updateCompany)
+
+router.delete('/',verifyToken,isAdmin,deleteCompany)
+router.post('/apply/:compId',verifyToken,applyCompanies)
 export default router;
